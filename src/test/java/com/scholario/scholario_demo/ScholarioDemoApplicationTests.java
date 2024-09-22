1f3def19f19b9c918600b6793dbcbb1084a64261
Fernando Álisson <fernando.alisson@ufpe.br>
2024-09-22 14:28:32 -0300

    start: Start the project

---
+++ src/test/java/com/scholario/scholario_demo/ScholarioDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scholario.scholario_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScholarioDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
