4bf54f05e0f75a189ef0cd337f123a21b8916ca1
Fernando Álisson <fernando.alisson@ufpe.br>
2024-10-12 22:22:43 -0300

    fix: fixxing the security configuration

--- app/backend/src/main/java/com/scholario/scholario_demo/security/SecurityConfig.java
+++ app/backend/src/main/java/com/scholario/scholario_demo/security/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.scholario.scholario_demo.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,10 +13,10 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
-        .csrf(csrf -> csrf.disable())  // Desabilitar CSRF para APIs REST
-        .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-            .anyRequest().authenticated() // Exigir autenticação para qualquer outra rota
-        ).httpBasic();
+        .csrf(AbstractHttpConfigurer::disable)
+        .sessionManagement(
+            session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        );
 
     return http.build();
   }
